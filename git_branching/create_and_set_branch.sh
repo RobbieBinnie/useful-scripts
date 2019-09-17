2f8519879cbe26960a78ee5a36d77cc359d5d302
robbie binnie <robbie.binnie@cloudreach.com>
2019-09-17 10:15:30 +0100

    removed additional forward slash

--- git_branching/create_and_set_branch.sh
+++ git_branching/create_and_set_branch.sh
@@ -1,7 +1,7 @@
 location="/scripts"
 mkdir -p $location
 
-cat ./create_and_set_branch.sh > "/$location/create_and_set_branch.sh"
+cat ./create_and_set_branch.sh > "$location/create_and_set_branch.sh"
 
 echo "
 alias setup-git='. $location/create_and_set_branch.sh'
