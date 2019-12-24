6deba5f0f93069c6d1cc3cb5e3b8e80e24276586
RobbieBinnie <16024497+RobbieBinnie@users.noreply.github.com>
2019-12-24 11:26:53 +0000

    Update Set-DomainJoin.ps1
    Added additional internal commentary to function

--- azure_functions/Set-DomainJoin.ps1
+++ azure_functions/Set-DomainJoin.ps1
@@ -20,6 +20,7 @@ function Set-DomainJoin {
 
     )
     Begin {
+        # check if a VM id is provided (and use it, if it is)
         if ($vmId -like "*subscriptions/*"){
             Write-Host "VM ID variable set"
             $resource = Get-AzResource -ResourceId $vmId
@@ -27,10 +28,12 @@ function Set-DomainJoin {
         }else {
             Write-Host "Using pipeline input"
         }
-
+        
+        # Get the password of the user that will carry out the domain joining for keyvault
         $sec = Get-AzKeyVaultSecret -VaultName $kv -Name $kvSecretName
         Write-Host "Got secret, trying to domain join..."
 
+        # create the JSON for the 'public' settings 
         $globalVars = '{
             "Name": "<name>",
             "OUPath": "<ou>",
@@ -42,6 +45,7 @@ function Set-DomainJoin {
         -replace "<user>","$username" `
         -replace "<ou>","$ouPath"
     
+        # create the JSON for the 'private' settings 
         $secretVars = '{
             "Password": "<pass>"
 }'`
@@ -51,14 +55,19 @@ function Set-DomainJoin {
         Write-Host "-------------------------------------------------------------------------------"
     }
     Process {
+        # loop here to allow the different ways in which piped inputs can be used
         foreach ($vm in $pipelineInput) {
+            # check if WhatIf flag is set
             if ($WhatIf -eq $false){
                 Write-Host "Starting domain join of: $($vm.name)"
+                # carry out domain join
                 set-AzVmExtension -resourceGroupName $vm.ResourceGroupName -ExtensionType "JsonADDomainExtension" `
                     -Name "joindomain" -Publisher "Microsoft.Compute" -TypeHandlerVersion "1.3" `
                     -VMName $vm.name -Location $vm.location -SettingString $globalVars `
                     -ProtectedSettingString $secretVars
             }else {
+                # Write out the vm names only if WhatIf flag is set
+                # Update this in the future to check DNS settings
                 Write-Host "Starting domain join of: $($vm.name) -WhatIf"
             }
         }
